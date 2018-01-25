359587928c69d5dadb6ae9a3280eb3f256adf5d0
kswisher <kenny.swisher@gmail.com>
2018-01-24 21:20:13 -0600

    added authentication middlewar

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {User} = require('./models/user');
 var {Todo} = require('./models/todo');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -131,6 +132,9 @@ app.post('/users', (req, res) => {
   });
 });
 
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
 
 app.listen(port,() => {
   console.log(`Started on port ${port}`);
