9c33d7f0c24ed2009690a811d58fdd85ef184d78
kswisher <kenny.swisher@gmail.com>
2018-01-31 22:25:33 -0600

    add POST /users/login

--- server/server.js
+++ server/server.js
@@ -114,9 +114,6 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 app.post('/users', (req, res) => {
-  var todo = new Todo({
-    text: req.body.text
-  });
   var reqUser = _.pick(req.body,['email', 'password']);
   var user = new User({
     email: reqUser.email,
@@ -134,6 +131,19 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  // pick the email and password off
+  var body = _.pick(req.body, ['email','password']);
+  // find the user
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.listen(port,() => {
