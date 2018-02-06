195f4040ede00dac88a0478691c37f1139898e9e
kswisher <kenny.swisher@gmail.com>
2018-02-05 20:46:01 -0600

    added DELETE /users/me/token for logout

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -299,4 +299,27 @@ describe('POST /users', () => {
   });
 });
 
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    // delete /users/me/token
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err,res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+    // set x-atuh equal to token
+    // 200
+    // find user, verify that tokens array has length of 0
+  });
+});
+
 });
